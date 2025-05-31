f6ae70f8df11fbabf524b047e287fe906ec6f887
AmiThaker <thakerami139@gmail.com>
2025-05-31 09:48:13 +0100

    Basics Day 1

---
+++ src/main/java/basics/IfElseExample.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class IfElseExample {
+
+    public static void main(String[] args) {
+        int score = 85;
+
+        if (score >= 90) {
+            System.out.println("Grade A");
+        } else if (score >= 75) {
+            System.out.println("Grade B");
+        } else {
+            System.out.println("Needs Improvement");
+        }
+    }
+}
---
+++ src/main/java/basics/VariablesDemo.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class VariablesDemo {
+
+    public static void main(String [] args){
+        int age=30;
+        double salary=65000.50;
+        String name="John";
+        boolean isWorking=true;
+
+        System.out.println(name+" is "+age+" years old");
+    }
+}
